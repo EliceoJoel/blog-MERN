832cfd938de4e35b6843ee3b8e22f5f96be3b81d
Elizeo <eliceoherbas@gmail.com>
2020-07-13 16:34:22 -0400

    mongo atlas connect

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./model/user.js');
+
+
 mongoose.connect('mongodb+srv://eliceoh:mongodb@mern-blog.25wrl.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log('DB connected'))
-                          .catch(err=>console.error(err));
+  {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=>console.log('DB connected'))
+  .catch(err=>console.error(err));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser())
 
 
  
-app.get('/', (req, res)=>{
-  res.send('hello world')
-});
+app.post('/api/users/register', (req, res)=>{
+    const user = new User(req.body)
+    user.save((err, userData)=>{
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+})
 
 
 app.listen(5000);
--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   name:{
